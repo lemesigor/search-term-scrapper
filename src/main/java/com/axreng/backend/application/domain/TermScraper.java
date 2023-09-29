58f3b341b1ae7cc639afd51565eaad60565b0bc6
Igor Lemes <igor.lemes@live.com>
2023-09-29 19:46:49 -0300

    feat: web scrapper in multiple pages

--- src/main/java/com/axreng/backend/application/domain/TermScraper.java
+++ src/main/java/com/axreng/backend/application/domain/TermScraper.java
@@ -1,6 +1,8 @@
 package com.axreng.backend.application.domain;
 
 import com.axreng.backend.shared.HtmlNormalizationHelper;
+import org.eclipse.jetty.util.log.Logger;
+import org.eclipse.jetty.util.log.Slf4jLog;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
@@ -8,10 +10,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -25,7 +30,7 @@ public class TermScraper {
     private Set<String> foundUrls;
 
     private Set<String> resultSet;
-
+    private Logger logger = new Slf4jLog("TermScraper")
 
 
     public TermScraper(String url, String term) {
@@ -45,25 +50,43 @@ public class TermScraper {
     }
 
     public void execute() {
-        scrape(this.url);
-    }
-    private void scrape(String currentUrl) {
-        final String httpsUrl = url;
+        ExecutorService executor = Executors.newFixedThreadPool(4);
 
-        Boolean foundTermInCurrentUrl = false;
+        executor.submit(() -> scrape(this.url));
+
+        executor.shutdown();
+
         try {
-            final URL urlObj = new URL(httpsUrl);
+            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            // Handle the exception
+            e.printStackTrace();
+        }
+    }
+
+    private void scrape(String currentUrl) {
+
+        try {
+            final URL urlObj = new URL(currentUrl);
 
             List<String> lines = this.getHtmlLines(urlObj);
 
 
-            if(!foundTermInCurrentUrl && findKeyword(lines, this.term)) {
-                System.out.println("achou");
-                this.resultSet.add(currentUrl.toString());
+            if (findKeyword(lines, this.term)) {
+                logger.info("achou");
+                this.resultSet.add(currentUrl);
+
+                logger.info("Result set atualizado: " + this.resultSet);
             }
 
-            System.out.println("****** Content of the URL ********");
 
+            Map<String, URL> internalUrls = findInternalUrls(lines, currentUrl);
+
+
+            for (URL internalUrl : internalUrls.values()) {
+                scrape(internalUrl.toString());
+            }
+            visitedUrls.add(currentUrl);
 
 
         } catch (MalformedURLException e) {
@@ -73,14 +96,61 @@ public class TermScraper {
         }
     }
 
+    private Map<String, URL> findInternalUrls(List<String> lines, String actualUrl) throws MalformedURLException {
+        Map<String, URL> internalUrls = new HashMap<>();
 
+        Pattern p = Pattern.compile("href=\"(.*?)\"", Pattern.DOTALL);
+
+        for (String line : lines) {
+            Matcher m = p.matcher(line);
+
+            while (m.find()) {
+                String urlStr = m.group().split("\"")[1];
+
+
+                if (!urlStr.endsWith(".html") || urlStr.startsWith("mailto")) {
+                    continue;
+                }
+                URI uri = URI.create(urlStr);
+
+                URL url = contructUrl(uri, actualUrl);
+
+                if (url == null) {
+                    continue;
+                }
+
+                urlStr = url.toString();
+                if (!this.foundUrls.contains(urlStr) && !this.visitedUrls.contains(urlStr) && !internalUrls.containsKey(urlStr)) {
+                    internalUrls.put(urlStr, url);
+                    this.foundUrls.add(urlStr);
+                }
+            }
+        }
+        return internalUrls;
+    }
+
+
+    private URL contructUrl(URI uri, String currentUrl) throws MalformedURLException {
+        String uriStr = uri.toString();
+
+        if (uri.isAbsolute()) {
+            if (uriStr.contains(currentUrl)) {
+                return new URL(uri.toString());
+            }
+        } else if (uriStr.startsWith("../")) {
+            return new URL(this.url + uri.toString().replace("../", ""));
+        } else {
+            return new URL(this.url + uri);
+        }
+
+        return null;
+    }
+
+    // achar keyword e url em um loop só
     private Boolean findKeyword(List<String> lines, String keyword) {
-        System.out.println(lines);
         String keywordPattern = "\\b" + keyword + "\\b";
         Pattern p = Pattern.compile(keywordPattern, Pattern.CASE_INSENSITIVE);
-        Boolean result = lines.stream().anyMatch(line -> p.matcher(line).find());
-        System.out.println(result);
-        return result;
+        return lines.stream().anyMatch(line -> p.matcher(line).find());
     }
 
 
@@ -89,6 +159,6 @@ public class TermScraper {
         BufferedReader htmlLines = new BufferedReader(new InputStreamReader(url.openStream()));
         Stream<String> lines = htmlLines.lines();
 
-        return lines.map(HtmlNormalizationHelper::normalizeHtml).collect(Collectors.toList());
+        return lines.collect(Collectors.toList());
     }
 }
