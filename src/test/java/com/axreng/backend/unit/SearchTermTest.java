9701b27d553ba293b854060b3c8401f87703dac3
Igor Lemes <igor.lemes@live.com>
2023-10-01 19:28:09 -0300

    test: add unit SearchTermTest

---
+++ src/test/java/com/axreng/backend/unit/SearchTermTest.java
@@ -0,0 +1,101 @@
+package com.axreng.backend.unit;
+
+import com.axreng.backend.application.domain.SearchStatus;
+import com.axreng.backend.application.domain.SearchTerm;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SearchTermTest {
+
+    private SearchTerm searchTerm;
+
+    private final String validWord = "ValidWord";
+
+    private final String smallInvalidWord = "Sh";
+
+    private final String bigInvalidWord = "ThisIsAReallyLongWordThatExceedsTheMaximumLengthOf32Characters";
+
+    @BeforeEach
+    void setUp() {
+        searchTerm = null;
+    }
+
+    @Test
+    void validWordShouldCreateSearchTermObject() {
+
+        searchTerm = new SearchTerm(validWord);
+
+        assertNotNull(searchTerm);
+        assertEquals(validWord, searchTerm.getWord());
+        assertNotNull(searchTerm.getId());
+    }
+
+    @Test
+    void newWordShouldHaveActiveStatus() {
+
+        searchTerm = new SearchTerm(validWord);
+
+        assertEquals(SearchStatus.active, searchTerm.getStatus());
+    }
+
+    @Test
+    void newWordShouldHaveEmptyUrls() {
+
+        searchTerm = new SearchTerm(validWord);
+
+        assertTrue(searchTerm.getUrls().isEmpty());
+    }
+
+    @Test
+    void invalidSmallWordShouldThrowIllegalArgumentException() {
+
+        assertThrows(IllegalArgumentException.class, () -> new SearchTerm(smallInvalidWord));
+    }
+
+    @Test
+    void invalidBigWordShouldThrowIllegalArgumentException() {
+
+        assertThrows(IllegalArgumentException.class, () -> new SearchTerm(bigInvalidWord));
+    }
+
+    @Test
+    void wordLengthGreaterThan32ShouldThrowIllegalArgumentException() {
+        String longWord = "ThisIsAReallyLongWordThatExceedsTheMaximumLengthOf32Characters";
+
+        assertThrows(IllegalArgumentException.class, () -> new SearchTerm(longWord));
+    }
+
+    @Test
+    void wordLengthEqualTo32ShouldCreateSearchTermObject() {
+        String validWord = "Exactly32CharactersLong";
+
+        searchTerm = new SearchTerm(validWord);
+
+        assertNotNull(searchTerm);
+        assertEquals(validWord, searchTerm.getWord());
+    }
+
+    @Test
+    void addUrlShouldAddUrlToUrls() {
+        String validWord = "ValidWord";
+        searchTerm = new SearchTerm(validWord);
+        String url = "https://example.com";
+
+        searchTerm.addUrl(url);
+
+        assertTrue(searchTerm.getUrls().contains(url));
+    }
+
+    @Test
+    void setStatusShouldSetStatus() {
+        String validWord = "ValidWord";
+        searchTerm = new SearchTerm(validWord);
+        SearchStatus newStatus = SearchStatus.done;
+
+        searchTerm.setStatus(newStatus);
+
+        assertEquals(newStatus, searchTerm.getStatus());
+    }
+}
