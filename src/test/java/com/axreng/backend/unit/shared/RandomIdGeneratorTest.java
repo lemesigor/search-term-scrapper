84c67ba872efbd1253f2b946ca12856286187fe1
Igor Lemes <igor.lemes@live.com>
2023-10-02 12:04:17 -0300

    test: RandomIdGeneratorTest

---
+++ src/test/java/com/axreng/backend/unit/shared/RandomIdGeneratorTest.java
@@ -0,0 +1,30 @@
+package com.axreng.backend.unit.shared;
+
+import com.axreng.backend.shared.RandomIdGenerator;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RandomIdGeneratorTest {
+
+    @Test
+    public void testGenerateRandomId() {
+        String randomId1 = RandomIdGenerator.generateRandomId(10);
+        String randomId2 = RandomIdGenerator.generateRandomId(20);
+
+        assertEquals(10, randomId1.length());
+        assertEquals(20, randomId2.length());
+
+        for (char c : randomId1.toCharArray()) {
+            assertTrue(isValidCharacter(c));
+        }
+        for (char c : randomId2.toCharArray()) {
+            assertTrue(isValidCharacter(c));
+        }
+    }
+
+    private boolean isValidCharacter(char c) {
+        String validCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        return validCharacters.indexOf(c) != -1;
+    }
+}
